d8fc05493ffd4130b9323ffbda2515fb34ca33b8
Dennis Bailey <bailey.dennis@gmail.com>
2016-05-25 14:24:46 -0600

    Added a landing page component

---
+++ src/js/components/landing/landing-controller.js
@@ -0,0 +1,11 @@
+angular
+  .module('troveApp')
+  .controller('LandingCtrl', LandingCtrl);
+
+LandingCtrl.$inject = ['$timeout', '$window', '$rootScope'];
+
+function LandingCtrl($timeout, $window, $rootScope) {
+  
+  var vm = this;
+
+}
