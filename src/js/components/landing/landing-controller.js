0d7802df4c25404327566e3f825f28dd490a9d84
Dennis Bailey <bailey.dennis@gmail.com>
2016-05-25 15:08:07 -0600

    Added a google geolocation request to the landing page controller

--- src/js/components/landing/landing-controller.js
+++ src/js/components/landing/landing-controller.js
@@ -2,10 +2,20 @@ angular
   .module('troveApp')
   .controller('LandingCtrl', LandingCtrl);
 
-LandingCtrl.$inject = ['$timeout', '$window', '$rootScope'];
+LandingCtrl.$inject = ['$timeout', '$window', '$rootScope', 'locationService'];
 
-function LandingCtrl($timeout, $window, $rootScope) {
+function LandingCtrl($timeout, $window, $rootScope, locationService) {
   
   var vm = this;
+  
+  vm.getLocation = function () {
+    locationService.getLocation()
+    .then( function (result) { $rootScope.lat = result.data.location.lat;
+                               $rootScope.lng = result.data.location.lng;  })
+    .catch( function (error) { return error; })
+    
+  };
+  
+  vm.getLocation();
 
 }
