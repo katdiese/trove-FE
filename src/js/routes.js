578f24c5795099238b76dac71dc1be3131c38c50
Dennis Bailey <bailey.dennis@gmail.com>
2016-05-27 11:01:18 -0600

    Undoing HTML 5 Mode, and removing commented out CORS debugging

--- src/js/routes.js
+++ src/js/routes.js
@@ -21,16 +21,7 @@
 
     .otherwise({redirectTo: '/'});
     
-    $locationProvider.html5Mode(true);
-
-/* 
- * Debugging CORS issues 
- */  
-//     $httpProvider.defaults.useXDomain = true;
-//     $httpProvider.defaults.withCredentials = true;
-//     delete $httpProvider.defaults.headers.common["X-Requested-With"];
-//     $httpProvider.defaults.headers.common["Accept"] = "application/json";
-//     $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
+//     $locationProvider.html5Mode(true);
 
   }
 
