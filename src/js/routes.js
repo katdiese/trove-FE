ddefcc131a0280b5e93f2a7f7f9e984a0a7e6bf4
Dennis Bailey <bailey.dennis@gmail.com>
2016-05-26 20:44:31 -0600

    WIP Fixing HTML5 Mode quirks when navigating directly to a page other than the root

--- src/js/routes.js
+++ src/js/routes.js
@@ -7,7 +7,7 @@
     .module('troveApp')
     .config(appConfig);
 
-    appConfig.$inject = ['$routeProvider', '$locationProvider', '$httpProvider'];
+  appConfig.$inject = ['$routeProvider', '$locationProvider', '$httpProvider'];
 
   function appConfig($routeProvider, $locationProvider, $httpProvider) {
     $routeProvider
@@ -20,9 +20,18 @@
     })
 
     .otherwise({redirectTo: '/'});
+    
     $locationProvider.html5Mode(true);
+
+/* 
+ * Debugging CORS issues 
+ */  
+//     $httpProvider.defaults.useXDomain = true;
+//     $httpProvider.defaults.withCredentials = true;
+//     delete $httpProvider.defaults.headers.common["X-Requested-With"];
+//     $httpProvider.defaults.headers.common["Accept"] = "application/json";
+//     $httpProvider.defaults.headers.common["Content-Type"] = "application/json";
+
   }
-
-
 
 })();
