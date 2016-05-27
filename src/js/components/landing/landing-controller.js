d100639bdcb1776d6e4169e2f8bd3a5380d5c125
Dennis Bailey <bailey.dennis@gmail.com>
2016-05-26 20:43:06 -0600

    Moved the assignment of rootscope variables to the location service. WIP Setup a locationservice call to the api to find nearby markets.

--- src/js/components/landing/landing-controller.js
+++ src/js/components/landing/landing-controller.js
@@ -9,11 +9,7 @@ function LandingCtrl($timeout, $window, $rootScope, locationService) {
   var vm = this;
   
   vm.getLocation = function () {
-    locationService.getLocation()
-    .then( function (result) { $rootScope.lat = result.data.location.lat;
-                               $rootScope.lng = result.data.location.lng;  })
-    .catch( function (error) { return error; })
-    
+    locationService.getLocation();    
   };
   
   vm.getLocation();
--- src/js/services/location-services.js
+++ src/js/services/location-services.js
@@ -9,7 +9,14 @@ function locationService($http, $window, $location, $rootScope) {
   return {
     
     getLocation: function() {
-      return $http.post('https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyDfPKtoUzLSeIRei104h3GCYjBe1th2vSs');
+      $http.post('https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyDfPKtoUzLSeIRei104h3GCYjBe1th2vSs')
+      .then( function (result) { $rootScope.lat = result.data.location.lat;
+                                 $rootScope.lng = result.data.location.lng;  })
+      .catch( function (error) { return error; });
+    },
+    
+    findNearbyMarkets: function(lat, lng, searchRadius) {
+      return $http.get('https://trove-api.herokuapp.com/v1/markets/nearby?lat=' + lat + '&lng=' + lng + '&searchRadius=' + searchRadius + '')
     }
   
   }
