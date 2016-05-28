23717695197b842c51eebe41762995c3151fe3ac
Kathy Dieser <katdiese@gmail.com>
2016-05-28 12:58:09 -0600

    assigned incoming API data to variable to pass into view

--- src/components/market-info/market-info.controller.js
+++ src/components/market-info/market-info.controller.js
@@ -7,11 +7,26 @@ MarketInfoCtrl.$inject = ['$timeout', '$window', '$rootScope', 'marketService'];
 function MarketInfoCtrl($timeout, $window, $rootScope, marketService) {
 console.log('market-info');
   var vm = this;
-  
+
+  vm.currMkt;
+  vm.currInfo;
+  vm.currMktCategories;
+
   vm.getMarketInfo = function() {
-    marketService.getMarketInfo();
+    marketService.getMarketInfo()
+    .then( function (result) {
+      console.log(result);
+      vm.currMkt = result;
+      vm.currInfo = result.data.info[0];
+      vm.currMktCategories = result.data.categories;
+      return result;
+    })
+    .catch( function (error) {
+      console.log(error);
+      return error;
+    });
   };
-  
+
   vm.getMarketInfo();
 
 
