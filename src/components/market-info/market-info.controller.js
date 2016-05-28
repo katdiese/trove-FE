6a78b5a60e8de6e01e9efe077f47bdcc200ad599
Dennis Bailey <bailey.dennis@gmail.com>
2016-05-28 11:35:25 -0600

    get info for one market works with the assumption of hard coded IDs on the server

--- src/components/market-info/market-info.controller.js
+++ src/components/market-info/market-info.controller.js
@@ -2,17 +2,17 @@ angular
   .module('troveApp')
   .controller('MarketInfoCtrl', MarketInfoCtrl);
 
-MarketInfoCtrl.$inject = ['$timeout', '$window', '$rootScope', 'locationService'];
+MarketInfoCtrl.$inject = ['$timeout', '$window', '$rootScope', 'marketService'];
 
-function MarketInfoCtrl($timeout, $window, $rootScope, locationService) {
+function MarketInfoCtrl($timeout, $window, $rootScope, marketService) {
 console.log('market-info');
   var vm = this;
-
-//   vm.getLocation = function () {
-//     locationService.getLocation();
-//   };
-// 
-//   vm.getLocation();
+  
+  vm.getMarketInfo = function() {
+    marketService.getMarketInfo();
+  };
+  
+  vm.getMarketInfo();
 
 
 }
--- src/js/services/market.services.js
+++ src/js/services/market.services.js
@@ -2,16 +2,16 @@ angular
   .module('troveApp')
   .service('marketService', marketService);
 
-marketService.$inject = ['$http', '$window', '$market', '$rootScope'];
+marketService.$inject = ['$http', '$window', '$rootScope'];
 
-function marketService($http, $window, $market, $rootScope) {
+function marketService($http, $window, $rootScope) {
     
   return {
     
     getMarketInfo: function(marketID) {
-      $http.get('https://trove-api.herokuapp.com/v1/markets/' + marketID + '')
-      .then( function (result) { return result; })
-      .catch( function (error) { return error; });
+      $http.get('https://trove-api.herokuapp.com/v1/markets/info')
+      .then( function (result) { console.log(result); return result; })
+      .catch( function (error) { console.log(error); return error; });
     }
   
   }
