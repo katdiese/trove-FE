1b13ae927ebb5bf078171bd3886e674dfe346008
Kathy Dieser <katdiese@gmail.com>
2016-05-28 12:57:29 -0600

    moved promise to controller

--- src/js/services/market.services.js
+++ src/js/services/market.services.js
@@ -5,15 +5,13 @@ angular
 marketService.$inject = ['$http', '$window', '$rootScope'];
 
 function marketService($http, $window, $rootScope) {
-    
+
   return {
-    
+
     getMarketInfo: function(marketID) {
-      $http.get('https://trove-api.herokuapp.com/v1/markets/info')
-      .then( function (result) { console.log(result); return result; })
-      .catch( function (error) { console.log(error); return error; });
+      return $http.get('https://trove-api.herokuapp.com/v1/markets/info');
     }
-  
+
   }
-  
+
 };
