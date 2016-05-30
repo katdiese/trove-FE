999101fff0886fd4270b7445ebfdb034513cb5df
Kathy Dieser <katdiese@gmail.com>
2016-05-30 12:11:05 -0600

    added functions to find zip based on form input, and clear zip when geolocation

--- src/components/landing/landing.controller.js
+++ src/components/landing/landing.controller.js
@@ -2,11 +2,23 @@ angular
   .module('troveApp')
   .controller('LandingCtrl', LandingCtrl);
 
-LandingCtrl.$inject = ['$timeout', '$window', '$rootScope', 'locationService'];
+LandingCtrl.$inject = ['$timeout','$location', '$window', '$rootScope', 'locationService'];
 
-function LandingCtrl($timeout, $window, $rootScope, locationService) {
+function LandingCtrl($timeout, $location, $window, $rootScope, locationService) {
 
   var vm = this;
+
+  vm.zip;
+
+  vm.clearZip = function() {
+    locationService.searchZip = "";
+    $location.path('/nearby');
+  }
+
+  vm.assignSearchZip = function() {
+    locationService.searchZip = vm.zip;
+    $location.path('/nearby');
+  }
 
   vm.getLocation = function () {
     locationService.getLocation();
