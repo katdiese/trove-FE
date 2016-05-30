f43a856c11b3e401c03ab42c197a4cb942ce50af
Kathy Dieser <katdiese@gmail.com>
2016-05-30 12:15:06 -0600

    added service query to database for mkts based on zip

--- src/js/services/location.services.js
+++ src/js/services/location.services.js
@@ -5,20 +5,26 @@ angular
 locationService.$inject = ['$http', '$window', '$location', '$rootScope'];
 
 function locationService($http, $window, $location, $rootScope) {
-    
+
   return {
-    
+
+    searchZip: "",
+
     getLocation: function() {
       $http.post('https://www.googleapis.com/geolocation/v1/geolocate?key=AIzaSyDfPKtoUzLSeIRei104h3GCYjBe1th2vSs')
       .then( function (result) { $rootScope.lat = result.data.location.lat;
                                  $rootScope.lng = result.data.location.lng;  })
       .catch( function (error) { return error; });
     },
-    
+
     findNearbyMarkets: function(lat, lng, searchRadius) {
       return $http.get('https://trove-api.herokuapp.com/v1/markets/nearby?lat=' + lat + '&lng=' + lng + '&searchRadius=' + searchRadius + '')
+    },
+
+    findMarketsByZip: function(zip) {
+      return $http.get('https://trove-api.herokuapp.com/v1/markets/nearbyzip?zip=' + zip + '')
     }
-  
+
   }
-  
+
 };
