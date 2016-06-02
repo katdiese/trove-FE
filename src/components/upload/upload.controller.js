918d964814b4debe413a48d25f954d05365bbf52
Kathy Dieser <katdiese@gmail.com>
2016-06-02 11:05:11 -0600

    s3 test

--- src/components/upload/upload.controller.js
+++ src/components/upload/upload.controller.js
@@ -15,10 +15,11 @@ angular
 
     vm.upload = function (file) {
         Upload.upload({
-            url: 'http://trove-api.herokuapp.com/v1/upload', //webAPI exposed to upload the file
+            url: 'https://trove-api.herokuapp.com/v1/upload', //webAPI exposed to upload the file
             data:{file:file} //pass file as data, should be user ng-model
         })
         .then(function (resp) { //upload function returns a promise
+            console.log(resp)
             if(resp.data.error_code === 0){ //validate success
                 $window.alert('Success ' + resp.config.data.file.name + 'uploaded. Response: ');
             } else {
